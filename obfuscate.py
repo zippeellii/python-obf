5e80d1aa649d02f028f159fb28f6e5d3b429e4e0
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2017-05-03 14:27:27 +0200

    Replace function call with appropriate mapping.

--- obfuscate.py
+++ obfuscate.py
@@ -6,15 +6,37 @@ file = fileinput.input()
 obf_file = open('output/obf.py', 'w')
 mapping = dict()
 
+lines = []
+
 for line in file:
-    m = re.search('(?<=def )\w+',line) 
+    lines.append(line)
+    # See if line includes a function definiton
+    m = re.search('(?<=def )\w+', line)
+    # See if line includes a variable instanciate 
+    n = re.search('(\w+)( = .*)', line)
+    for k, v in mapping.iteritems():
+        # Does line contain a stored keyword for a function call
+        w = re.search(k +'\(', line)
+        if w:
+            line = line.replace(k, v)
     if m:
         function_name = m.group(0)
         random_name = utils.gen_random_name()
         mapping[function_name] = random_name
-        obf_file.write(line.replace(function_name, random_name))
-    else:
-        obf_file.write(line)
+        line.replace(function_name, random_name)
+        line = line.replace(function_name, random_name)
+    elif n:
+        variable_name = n.group(1)
+        random_name = utils.gen_random_name()
+        mapping[variable_name] = random_name
+        line = line.replace(variable_name, random_name)
+
+    obf_file.write(line)
+
 
 def test():
-    print 'Hello'
+    a = 'Hello'
+    a = 'None'
+    print a
+
+test()
