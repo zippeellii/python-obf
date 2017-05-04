05f86d93de4b1af3bfb3c14628add25fef8448c8
Johan Backman <johback@student.chalmers.se>
2017-05-04 09:18:26 +0200

    Improve boilerplate code

---
+++ src/obfuscator.py
@@ -0,0 +1,38 @@
+"""Usage: obfuscator.py [-o NAME] FILE...
+
+Arguments:
+    FILE        python file that should be obfuscated
+
+Options:
+    -o NAME     output into a file named NAME.i
+"""
+
+import docopt
+
+
+def _rename_functions(src):
+    """Hello."""
+    pass
+
+
+def _remove_comments(src):
+    """Hello."""
+    pass
+
+
+def _add_fuzzed_code(src):
+    """Hello."""
+    pass
+
+
+def _random_ordering(src):
+    """Hello."""
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        args = docopt.docopt(__doc__)
+        print "hello"
+    except docopt.DocoptExit as e:
+        print e.message
