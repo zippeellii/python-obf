6bd87a32c3563bb8b2b7ea32d0d4fa2c42b69fe9
Johan Backman <johback@student.chalmers.se>
2017-05-10 10:12:50 +0200

    Add more random algorithms

--- data/random_code.py
+++ data/random_code.py
@@ -81,3 +81,134 @@ def wiggle_sort(nums):
     for i in range(len(nums)):
         if (i % 2 == 1) == (nums[i-1] > nums[i]):
             nums[i-1], nums[i] = nums[i], nums[i-1]
+
+def backtrack(pattern, string, dic):
+    print(dic)
+    if len(pattern) == 0 and len(string) > 0:
+        return False
+    if len(pattern) == len(string) == 0:
+        return True
+    for end in range(1, len(string)-len(pattern)+2):
+        if pattern[0] not in dic and string[:end] not in dic.values():
+            dic[pattern[0]] = string[:end]
+            if backtrack(pattern[1:], string[end:], dic):
+                return True
+            del dic[pattern[0]]
+        elif pattern[0] in dic and dic[pattern[0]] == string[:end]:
+            if backtrack(pattern[1:], string[end:], dic):
+                return True
+    return False
+
+def shortest_distance(grid):
+    if not grid or not grid[0]:
+        return -1
+
+    matrix = [[[0,0] for i in range(len(grid[0]))] for j in range(len(grid))]
+
+    count = 0    # count how many building we have visited
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                bfs(grid, matrix, i, j, count)
+                count += 1
+
+    res = float('inf')
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j][1]==count:
+                res = min(res, matrix[i][j][0])
+
+    return res if res!=float('inf') else -1
+
+def bfs(grid, matrix, i, j, count):
+    q = [(i, j, 0)]
+    while q:
+        i, j, step = q.pop(0)
+        for k, l in [(i-1,j), (i+1,j), (i,j-1), (i,j+1)]:
+            # only the position be visited by count times will append to queue
+            if 0<=k<len(grid) and 0<=l<len(grid[0]) and \
+                    matrix[k][l][1]==count and grid[k][l]==0:
+                matrix[k][l][0] += step+1
+                matrix[k][l][1] = count+1
+                q.append((k, l, step+1))
+
+def bracket(words, symbols):
+    root = TrieNode()
+    for s in symbols:
+        t = root
+        for char in s:
+            if char not in t.c:
+                t.c[char] = TrieNode()
+            t = t.c[char]
+        t.sym = s
+    result = dict()
+    for word in words:
+        i = 0
+        symlist = list()
+        while i < len(word):
+            j, t = i, root
+            while j < len(word) and word[j] in t.c:
+                t = t.c[word[j]]
+                if t.sym is not None:
+                    symlist.append((j+1-len(t.sym), j+1, t.sym))
+                j += 1
+            i += 1
+        if len(symlist) > 0:
+            sym = reduce(lambda x, y: x if x[1]-x[0] >= y[1]-y[0] else y, symlist)
+            result[word] = "{}[{}]{}".format(word[:sym[0]], sym[2], word[sym[1]:])
+    return tuple(word if word not in result else result[word] for word in words)
+
+def serialize(root):
+    def build_string(node):
+        if node:
+            vals.append(str(node.val))
+            build_string(node.left)
+            build_string(node.right)
+        else:
+            vals.append("#")
+    vals = []
+    build_string(root)
+    return " ".join(vals)
+
+
+def deserialize(data):
+    def build_tree():
+        val = next(vals)
+        if val == "#":
+            return None
+        node = TreeNode(int(val))
+        node.left = build_tree()
+        node.right = build_tree()
+        return node
+    vals = iter(data.split())
+    return build_tree()
+
+def count_elem(array, query):
+    def first_occurance(array, query):
+        lo, hi = 0, len(array) -1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if (array[mid] == query and mid == 0) or \
+                (array[mid] == query and array[mid-1] < query):
+                return mid
+            elif (array[mid] <= query):
+                lo = mid + 1
+            else:
+                hi = mid - 1
+    def last_occurance(array, query):
+        lo, hi = 0, len(array) -1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if (array[mid] == query and mid == len(array) - 1) or \
+                (array[mid] == query and array[mid+1] > query):
+                return mid
+            elif (array[mid] <= query):
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+    first = first_occurance(array, query)
+    last = last_occurance(array, query)
+    if first is None or last is None:
+        return None
+    return last - first + 1
